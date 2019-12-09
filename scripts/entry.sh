4525f062a7300bbc19bf472a859819a12d59491a
yacht7 <wyatt.gill@outlook.com>
2019-12-09 07:22:46 -0600

    Fix routing to LAN

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -90,13 +90,15 @@ iptables -P OUTPUT DROP
 
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
+iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
+
 iptables -A INPUT -s 192.168.0.0/24 -j ACCEPT
 iptables -A OUTPUT -d 192.168.0.0/24 -j ACCEPT
 
-iptables -A INPUT -s 255.255.255.255 -j ACCEPT
-iptables -A OUTPUT -d 255.255.255.255 -j ACCEPT
+iptables -A OUTPUT -d 193.138.218.74 -j ACCEPT
 
+iptables -A INPUT -i tun+ -j ACCEPT
 iptables -A OUTPUT -o tun+ -j ACCEPT
 
 # for list of ports see the following link:
@@ -107,7 +109,8 @@ for ip in $(nslookup $domain localhost | tail -n +4 | grep -Eo '\d{1,3}\.\d{1,3}
     iptables -A OUTPUT -o eth+ -d $ip -p udp -m multiport --dports 53,1194:1197,1300:1303,1400 -j ACCEPT
 done
 
-iptables -A OUTPUT -o eth+ ! -d 193.138.218.74 -p tcp --dport 53 -j DROP
+ip route add 192.168.0.0/24 via $(ip r | grep 'default via' | cut -d " " -f 3) dev eth0
+
 echo -e "[INFO] iptables rules created\n"
 
 sleep 5
